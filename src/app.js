4958b26eb469ff40b4912fb95fe6c71e579ea5c5
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-31 12:49:15 +0530

    app done, express funcstionss

--- src/app.js
+++ src/app.js
@@ -11,7 +11,13 @@ app.use(cors({
 
 app.use(express.json({
    limit: "16kb",
-
 }))
+
+app.use(express.urlencoded({
+   extended: true,
+   limit: "16kb",
+}))
+
+app.use(express.static("public"))
 
 export {app}
