c884358969edfcbdc99964a144fd07463e4d000f
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-31 12:45:53 +0530

    done

--- src/app.js
+++ src/app.js
@@ -3,5 +3,15 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express()
-app.use(cors())
+app.use(cors({
+   origin: process.env.CORS_ORIGIN,
+   credentials: true,
+
+}))
+
+app.use(express.json({
+   limit: "16kb",
+
+}))
+
 export {app}
